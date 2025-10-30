35b7582f58b127c22202fa97108c91da775ce085
Mihitha01 <mihithabandara@gmail.com>
2025-10-30 23:39:06 +0530

    elif

--- code_with_mosh/if.py
+++ code_with_mosh/if.py
@@ -1,12 +1,15 @@
 is_hot = False
+is_cold = True
 
 if is_hot:
     print("It's a hot day")
     print("Drink plenty of water")
 
-
-else:
+elif is_cold:
     print("It's a cold day")
     print("Wear worm clothes")
-    
+
+else:
+    print("It's a lovely day")
+
 print("Enjoy your day")
