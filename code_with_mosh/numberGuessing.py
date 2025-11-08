a7e2949e3fff0744658098e621155cbbb8d02585
Mihitha01 <mihithabandara@gmail.com>
2025-11-08 23:56:05 +0530

    basic number gussing game

--- code_with_mosh/numberGuessing.py
+++ code_with_mosh/numberGuessing.py
@@ -1,8 +1,23 @@
-guess = input("Number you chose is : ")
-print(guess)
+# simple_number_guess.py
+import random
 
-if 12 :
-    print("Correct")
-else :
-    print("False")
-    
+number = random.randint(1, 100)
+attempts = 0
+
+print("Guess the number between 1 and 100!")
+
+while True:
+    try:
+        guess = int(input("Your guess: "))
+    except ValueError:
+        print("Please enter a whole number.")
+        continue
+
+    attempts += 1
+    if guess < number:
+        print("Too low.")
+    elif guess > number:
+        print("Too high.")
+    else:
+        print(f"Correct! You found the number in {attempts} attempts.")
+        break
