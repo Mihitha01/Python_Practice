c2d4eb9657c2a511c5d436ba4edcc34424d086ea
Mihitha01 <mihithabandara@gmail.com>
2025-11-09 14:25:47 +0530

    initial structure

---
+++ code_with_mosh/cargame.py
@@ -0,0 +1,8 @@
+command = ""
+
+while command.lower() != "quit":
+    command = input("> ")
+    if command.lower() == "start":
+        print("Car Started...")
+    elif commmand.lower() == "stop":
+        print("Car Stopped...")
