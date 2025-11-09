581412c74b7ffd92de3c4830a57c8e217b0239a2
Mihitha01 <mihithabandara@gmail.com>
2025-11-09 14:28:18 +0530

    added help part to the code

--- code_with_mosh/cargame.py
+++ code_with_mosh/cargame.py
@@ -1,8 +1,14 @@
 command = ""
 
 while command.lower() != "quit":
-    command = input("> ")
-    if command.lower() == "start":
+    command = input("> ").lower()
+    if command == "start":
         print("Car Started...")
-    elif commmand.lower() == "stop":
+    elif command == "stop":
         print("Car Stopped...")
+    elif command == "help":
+        print("""
+            start - to start the car
+            stop - to stop the car
+            quit - to quit    
+        """)
