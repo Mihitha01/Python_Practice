76a5048a4064a96dc436f7b4186942c71b585ddb
Mihitha01 <mihithabandara@gmail.com>
2025-11-29 18:00:25 +0530

    add list manipulation methods in list_methods.py; create tuples.py and unpacking.py files

--- code_with_mosh/list_methods.py
+++ code_with_mosh/list_methods.py
@@ -12,3 +12,19 @@ print(numbers)
 
 numbers.pop() # remove the last item
 print(numbers)
+
+print(10 in numbers)
+print(50 in numbers)
+
+numbers.sort() #ascending order sort
+print(numbers)
+
+numbers.reverse()
+print(numbers) #reverse order sort
+
+num = [2, 3, 4, 6, 3, 4, 6, 1]
+u = []
+for n in num:
+    if n not in u:
+        u.append(n)
+print(u)
---
+++ code_with_mosh/tuples.py
@@ -0,0 +1,2 @@
+numbers = (1 , 2, 3) #can't modify
+print(numbers[0])
