3cdc9ff677375addcc594dcae61657a47bf23300
Mihitha01 <mihithabandara@gmail.com>
2025-11-29 16:24:25 +0530

    add matrix initialization and item manipulation in list_2d.py; implement list methods in list_methods.py

---
+++ code_with_mosh/list_methods.py
@@ -0,0 +1,14 @@
+numbers = [5, 2, 1, 7, 4]
+numbers.append(20) #add number to the end of the list
+print(numbers)
+
+numbers.insert(0, 10)
+numbers.insert(1, 20)
+print(numbers)
+
+numbers.remove(5) # remove any item from the list
+numbers.remove(2)
+print(numbers)
+
+numbers.pop() # remove the last item
+print(numbers)
