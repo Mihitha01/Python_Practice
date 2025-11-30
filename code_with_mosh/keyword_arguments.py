03ee4d054d632e1cfe092b31713b15e84ee89009
Mihitha01 <mihithabandara@gmail.com>
2025-11-30 19:59:32 +0530

    add greet_user function with keyword argument demonstration in keyword_arguments.py; create return.py file

---
+++ code_with_mosh/keyword_arguments.py
@@ -0,0 +1,12 @@
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome aboard')
+
+
+print("Start")
+greet_user(last_name="Smith", first_name="John")
+#calc_cost(total=50, shipping=5, discount=0.1) this is a good keyword argument
+print("Finish")
+
+#keyword argument comes after positional argument
+
