254c28b59a4eea19e16cc70b7a40cbc4cdc65deb
Mihitha01 <mihithabandara@gmail.com>
2025-12-10 23:47:28 +0530

    added multiple operators

--- code_with_mosh/return.py
+++ code_with_mosh/return.py
@@ -1,6 +1,12 @@
 def square(number):
     return number * number
 
+def add(number):
+    return number + number
+
+def div(number):
+    return number / number
+
 print(square(5 * 5))
 print(square(5 + 5))
-print(square(5 - 5))
+print(square(5 / 5))
