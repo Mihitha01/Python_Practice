72bf96236f1f41962ce8756680a8c425be2dc123
Mihitha01 <mihithabandara@gmail.com>
2026-03-27 12:42:01 +0530

    Add Random Forest regression model for insurance charges prediction

---
+++ model_revision/randomForest01.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+data = pd.read_csv("data/insurance.csv")
+
+data = pd.get_dummies(data, drop_first=True)
+
+X = data.drop("charges", axis=1)
+y = data["charges"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
