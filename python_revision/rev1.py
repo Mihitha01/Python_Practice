98cbb39911195210281032c92b1e6d24ffd40c12
Mihitha01 <mihithabandara@gmail.com>
2026-03-27 10:10:16 +0530

    Add initial Python scripts for basic print functionality and string manipulation

---
+++ python_revision/rev1.py
@@ -0,0 +1,16 @@
+print("Hello, World!")
+
+name = "Himath"
+age = 22
+height = 1.76
+is_student = True
+
+print(name)
+print(age,height,is_student)
+
+x = "25"
+y = int(x)
+z = float("3.5")
+
+print(y + 5)
+print(z * 3)
---
+++ python_revision/rev2.py
@@ -0,0 +1,9 @@
+message = "Python Programming"
+
+print(message.upper())
+print(message.lower())
+print(message.replace("Python", "Java"))
+print(len(message))
+print(message[0])
+print(message[-1])
+
