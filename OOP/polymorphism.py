1656013bf06c0b3105bf017729f6a7242ba0943a
Mihitha01 <mihithabandara@gmail.com>
2026-03-29 14:31:22 +0530

    Refactor shape class to include abstract area method and implement area methods for Circle, Square, and Triangle classes

--- OOP/polymorphism.py
+++ OOP/polymorphism.py
@@ -1,17 +1,34 @@
 from abc import ABC, abstractmethod
 
 class shape:
-    pass
+        
+    @abstractmethod
+    def area(self) -> float:    
+        pass
 
 class Circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
 
-    @abstractmethod
     def area(self):
-        pass
-    
-class Square(shape):
-    pass
-class Triangle(shape):
-    pass
+        return 3.14 * self.radius ** 2
 
-shapes = [Circle(), Square(), Triangle()]
+class Square(shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Triangle(shape):      
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height    
+
+shapes = [Circle(5), Square(4), Triangle(3, 6)]
+
+for s in shapes:
+    print(s.area())
