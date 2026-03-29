4cd04a2d68fd712f413fd931e5b8b95d6436a3cd
Mihitha01 <mihithabandara@gmail.com>
2026-03-29 20:34:57 +0530

    Add initial implementation of for loops to iterate over lists

---
+++ python_revision/for.py
@@ -0,0 +1,15 @@
+# for variable in sequence:
+
+numbers = [1,2,3,4,5]
+
+for num in numbers:
+    print(num)
+
+string = ["Amil","Kamal","Nadun"]
+
+for names in string:
+    print(names)
+
+for i in range(5):
+    print(i)
+
---
+++ python_revision/for2.py
@@ -0,0 +1,4 @@
+fruits = ["Apple","Orange","Mango"]
+
+for index, fruit in enumerate(fruits):
+    
