d74ec2fe14116d00d93e771ade90377b7591d760
Mihitha01 <mihithabandara@gmail.com>
2026-03-29 14:44:39 +0530

    Add initial implementation of conditional statements for grading and legal age verification

---
+++ python_revision/if.py
@@ -0,0 +1,19 @@
+marks = 35
+'''
+if marks >= 75:
+    print("A Grade")
+
+elif marks >= 40:
+    print("Passed")
+
+else:
+    print("Failed")
+'''
+
+age = 20
+has_id = True
+
+if age >= 18 and has_id:
+    print("Legal")
+else:
+    print("Illegal")
