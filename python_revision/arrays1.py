f12e86894d27a1f03ca062000e493e175068ed26
Mihitha01 <mihithabandara@gmail.com>
2026-03-29 22:36:28 +0530

    Add initial implementations for list and NumPy array operations

---
+++ python_revision/arrays1.py
@@ -0,0 +1,12 @@
+#this is a list of numbers
+
+numbers = [1,2,3,4,5]
+
+print(numbers[0])
+print(numbers[-1])
+
+numbers.append(6)
+print(numbers)
+
+numbers.remove(2)
+print(numbers)
---
+++ python_revision/arrays_for_ml.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+
+print(arr[2])
+
+arr = arr*2 #assign 
+print(arr)
+
