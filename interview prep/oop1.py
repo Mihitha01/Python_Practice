f19b281200b76a90d004e637375b3d6f661d5e75
Mihitha01 <mihithabandara@gmail.com>
2026-04-05 15:54:43 +0530

    Fix formatting and add example usage for BankAccount class

--- interview prep/oop1.py
+++ interview prep/oop1.py
@@ -4,6 +4,13 @@ class BankAccount:
 
     def deposite(self, amount):
         self.__balance += amount
-    
+
     def get_balance(self):
         return self.__balance
+    
+acc = BankAccount(1000)
+print(acc.get_balance())
+
+acc.deposite(1000)
+
+print(acc.get_balance())
