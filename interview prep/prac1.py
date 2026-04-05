bc57a17f749e4e6a83c298b4c63b3b9b5b17340a
Mihitha01 <mihithabandara@gmail.com>
2026-04-05 12:59:33 +0530

    Enhance is_palindrome function documentation and add class structure for A and B

--- interview prep/prac1.py
+++ interview prep/prac1.py
@@ -14,6 +14,19 @@ print(a)
 s="leval"
 
 def is_palindrome(s):
-    return s==s[::-1]
+    return s==s[::-1] # s[start:stop:step]
+                      # start -> where to begin , stop -> where to stop , step -> how to move
 
 print(is_palindrome(s))
+
+class A:
+    def show(self):
+        print("A")
+
+
+class B(A):
+    def show(self):
+        print("B")
+
+obj = B()
+obj.show()        
